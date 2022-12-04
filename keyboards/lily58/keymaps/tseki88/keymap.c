cd4fd779da624e3674b7348afaa4f1c677bb9422
Steve Seki <teruhisaseki@gmail.com>
2022-12-04 16:26:16 -0500

    add media control

--- keyboards/lily58/keymaps/tseki88/keymap.c
+++ keyboards/lily58/keymaps/tseki88/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,         KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   KC_TAB,         KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
   LCTL_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,        KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,   KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENTER),
+  KC_LSFT,        KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_PGUP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENTER),
                             KC_LGUI, KC_LALT, MO(_LOWER), LCTL_T(KC_SPC),    KC_BSPC, MO(_RAISE), KC_LGUI, KC_RGUI
 ),
 
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
   KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_DLR,  KC_AMPR, KC_PIPE, KC_LCBR, KC_RCBR, _______,
   _______,  KC_F2, KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSLS, XXXXXXX,
-  _______,  KC_F8, KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_PGUP, KC_PGDN,  XXXXXXX, KC_HOME, KC_END,  KC_LBRC, KC_RBRC, XXXXXXX,
+  _______,  KC_F8, KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  XXXXXXX, KC_HOME, KC_END,  KC_LBRC, KC_RBRC, XXXXXXX,
                             _______, _______, _______,  _______, KC_DEL,   _______, _______, _______
 ),
 
@@ -56,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT(
   KC_TRNS,_______, _______, _______, _______, _______,                   _______, KC_NO,   KC_NO, KC_NO, XXXXXXX, _______,
-  KC_TILD,KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_PSLS, KC_P7,   KC_P8, KC_P9, KC_PMNS, XXXXXXX,
+  XXXXXXX,KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_MUTE,                   KC_PSLS, KC_P7,   KC_P8, KC_P9, KC_PMNS, XXXXXXX,
   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_PAST, KC_P4,   KC_P5, KC_P6, KC_PPLS, XXXXXXX,
-  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_P0,   KC_P1,   KC_P2, KC_P3, XXXXXXX, XXXXXXX,
+  KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_P0,   KC_P1,   KC_P2, KC_P3, KC_NUM, XXXXXXX,
                             _______, _______, _______, _______, _______, _______, KC_PDOT, _______
 ),
 
